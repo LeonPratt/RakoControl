855e3b36d437cf89dc73d359014796c46091ce44
LeonPratt <leon.alex.pratt@gmail.com>
2022-04-17 16:40:46 +0100

    bug fixes and additional features

---
+++ playback.py
@@ -0,0 +1,164 @@
+import datetime
+import sched
+import time
+import socket
+import logging
+import os
+from os import walk
+
+from numpy import full
+
+from threading import Thread
+#enter the names of the log files
+lognames = ["rako-2022-03-31.log","rako-2022-04-31.log"]
+
+def Current_Time():
+
+    global current_time
+    while True:
+        t=datetime.datetime.now(datetime.timezone.utc)
+        T=t.astimezone().isoformat(timespec='seconds')
+        current_time = T.split("T")[1]
+        time.sleep(1)
+
+thread = Thread(target=Current_Time)
+thread.start()
+
+def set_scene(dta):
+
+    room = int(dta[2])
+    channel = int(dta[3])
+    scene =int(dta[4])
+
+
+    BRIDGE_IP = "192.168.1.34"
+    PORT = "9761"
+    data = bytearray.fromhex('000000000000000000')
+
+    data[0] = 0x52
+    data[1] = 7
+    data[2] = room >> 8
+    data[3] = room & 0xff
+    data[4] = channel
+    data[5] = 0x31
+    data[6] = 0x1
+    data[7] = scene
+
+    sum = 0
+    for x in data[1:]:
+        sum += x
+
+    data[8] = 0x100 - (sum & 0xff)
+
+    print(data)
+    soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+    soc.connect((BRIDGE_IP, PORT))
+    soc.send(data)
+    soc.close()
+
+#def playback(full_log):
+#
+#    info = []
+#
+#
+#    for x in range(len(full_log)):
+#        current_time = ""
+#        date = full_log[x].split(" ")[0]
+#        tod = date.split("T")[1] 
+# 
+#        splt = full_log[x].split(" ")
+#        room = splt[5].split("=")[1]
+#        channel = splt[7].split("=")[1]
+#        scene = splt[8].split("=")[1]
+#        event = splt[1]            
+#        dta = (tod, event,room,channel,scene)
+#        info.append(dta)            
+#        continue
+#    
+#    yield info
+
+
+def breakup_log(log):
+    
+    date = log.split(" ")[0]
+    tod = date.split("T")[1] 
+ 
+    splt = log.split(" ")
+    room = splt[5].split("=")[1]
+    channel = splt[7].split("=")[1]
+    scene = splt[8].split("=")[1]
+    event = splt[1]            
+    dta = (tod, event,room,channel,scene)
+           
+    yield dta
+
+
+def schedule_lights(lst):
+    c = current_time.split("+")[0]
+    currentdatetime = datetime.datetime.strptime(c, "%H:%M:%S")
+    s = sched.scheduler(time.time,time.sleep)
+    for event in lst:
+        TIME = event[0]
+        t = TIME.split("+")[0]
+        DateTime = datetime.datetime.strptime(t, "%H:%M:%S")
+        delay = (DateTime-currentdatetime).total_seconds()
+        print(delay)
+
+        #s.enter(delay,1,set_scene, argument= (event))
+
+
+def strip_earlier(lst):
+    
+    for x in range(len(lst)):
+        if lst[0][0] < current_time:
+            lst.pop(0)
+        elif lst[0][0] > current_time:
+            break
+
+    yield lst
+
+
+def readlogs():
+
+    logs = []
+
+    path = r"\rako_record"
+    dir_path= os.path.dirname(os.path.realpath(__name__)) + path
+
+    filenames = next(walk(dir_path))[2]
+    lognames = []
+    for x in range(len(filenames)):
+        if ".log" in filenames[x]:
+            lognames.append(filenames[x])
+    lognames.sort()
+
+
+   
+    global full_log
+    full_log = []
+
+    for x in range(len(lognames)):
+        fn = "rako_record\\"+ lognames[x]
+
+        f = open(fn, "r")
+        file = f.readlines()
+        for y in range(len(file)):
+
+            if " I " in file[y]:
+                full_log.append(file[y].strip("\n"))
+
+
+    for x in range(len(full_log)):
+        inf = breakup_log(full_log[x])
+        logs.append(next(inf))
+    inf = strip_earlier(logs)
+    logs = next(inf)
+
+    schedule_lights(logs)
+
+
+while True:    
+    readlogs()
+    time.sleep(10)
+
+        
---
+++ rako_holiday.py
@@ -0,0 +1,339 @@
+import socket
+import random
+import http.client
+import datetime
+import pytz
+import ephem
+import time
+import pdpyras
+import schedule
+import traceback
+import sys
+
+alarm_delay = 5  # don't send pagerduty alert until alarm has been sounding for X seconds
+
+UDP_IP = "0.0.0.0"
+BRIDGE_IP = '192.168.1.34'
+PORT = 9761
+URL = 'http://' + BRIDGE_IP +'/rako.xml'
+
+routing_key = 'R03DGSERQAG8S08Y18MMNQ8WSP33ND5H'
+warn_routing_key = 'R03DJ1B4QUTG5NDO897WFQ2PDEUIVDSL'
+
+path = "/var/opt/data/"
+
+hidden_rooms = { 0 : "Master_Control", 50 : "Front_Porch_PIR", 52 : "Alarm_Interface" }
+
+non_interactive = hidden_rooms.keys() | { 33 : "Front_Entrance_Pillars", 32 : "Gate_Lights" }.keys()
+
+warn_window = 4   # 180 min centered around sunset 
+
+def get_room_names():
+    import xmltodict
+    import urllib.request
+
+    global xrooms, interactive
+
+    xml = ""
+    weburl=urllib.request.urlopen(URL)
+    if(weburl.getcode() == 200):
+        xml=weburl.read()
+
+    dict=xmltodict.parse(xml)
+
+    xrooms=hidden_rooms
+    interactive = set()
+    lstRoom_num = []
+    for room in dict['rako']['rooms']['Room'] :
+        if room['@id'] == '0':
+            continue
+        room_name = room["Title"].replace(" ", "_")
+
+        #print(room)
+        r = int(room['@id'])
+        print("%02d %s" % (r,room_name) )
+
+        lstRoom_num.append(r)
+
+        xrooms[r]=room_name
+
+        if r not in non_interactive:
+            interactive.add(r)
+
+
+log_file = 0
+
+def new_file():
+    global log_file
+    tmp = log_file
+    t=datetime.datetime.now(datetime.timezone.utc)
+    global name
+    name = path + "rako-%s.log"%t.astimezone().isoformat(timespec='seconds')
+
+    if sys.platform == "win32":
+        name = "deleteme.log" ##XXXXXXXXXXXXXXXXXXXXXXXXXX
+
+    log_file = open(name,"w+", 1) # line bufferd
+    if tmp:
+        log_file.close()     
+
+obs = ephem.Observer()
+obs.lat = "52.2053"
+obs.long="0.1218"
+
+alarm_set = "unset"
+
+def check_alarm_still_set_at_0620():
+    global alarm_set, log_session
+    t=datetime.datetime.now(datetime.timezone.utc)
+    T=t.astimezone().isoformat(timespec='seconds')
+    log_file = open(name, "w+")
+    print(T, "check_alarm_set_at_0620")
+    if alarm_set == "night":
+        alarm_set = "full"
+        print(T, "alarm_set_upgrade", alarm_set)
+        log_file.write( "%s alarm_set_upgrade %s\n" % (T,alarm_set) ) 
+        resp = log_session.submit("Alarm set upgrade"+ alarm_set, 'Elmhurst')
+
+warn_alarm_unset = False
+last_interactive = datetime.datetime.now(datetime.timezone.utc)
+
+def check_if_alarm_set():
+    global warn_event_key, warn_alarm_unset, last_interactive
+    t=datetime.datetime.now(datetime.timezone.utc)
+    T=t.astimezone().isoformat(timespec='seconds')
+
+    print(T, "check_if_alarm_set", alarm_set, last_interactive, t )
+    if alarm_set == "unset" and last_interactive + datetime.timedelta(minutes=warn_window) < t:
+        print(T, "alarm_unset_warning_pagerduty")
+        warn_event_key = warn_event_session.trigger("Alarm unset but house unoccupied", 'Elmhurst')
+        warn_alarm_unset = True
+    return schedule.CancelJob    # run once
+
+def get_sunrise_and_set():
+    global obs
+    global sunrise
+    global sunset 
+    tz = pytz.timezone("UTC")
+    obs.date= ("%s 12:00" % datetime.date.today() )
+    sunrise=tz.localize(obs.previous_rising(ephem.Sun()).datetime())
+    sunset =tz.localize(obs.next_setting(ephem.Sun()).datetime()) 
+
+    t=datetime.datetime.now(datetime.timezone.utc)
+    T=t.astimezone().isoformat(timespec='seconds')
+    print( T, "ephem_calc", sunrise, sunset )
+
+    check_at = sunset + datetime.timedelta(minutes=warn_window/2)
+    schedule.every().day.at(check_at.strftime("%H:%M:%S")).do(check_if_alarm_set)
+
+def listening():
+    soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+    soc.bind((UDP_IP, PORT))
+    soc.settimeout(1.0)
+           
+    global log_file 
+    
+    holdoff=0
+    global log_session, event_key, event_session
+    event_key = ''
+    log_session = pdpyras.ChangeEventsAPISession(routing_key)
+    event_session = pdpyras.EventsAPISession(routing_key)
+    resp = ''
+    
+    global warn_event_key, warn_log_session, warn_event_session, warn_alarm_unset
+    warn_event_key = ''
+    warn_log_session = pdpyras.ChangeEventsAPISession(warn_routing_key)
+    warn_event_session = pdpyras.EventsAPISession(warn_routing_key)
+
+    global sunrise, sunset
+    
+    global last_interactive
+    
+    ######  play recorded events if alarm_set == "full" or warn_event_key 
+
+    while True:    
+        try:
+            data, addr = soc.recvfrom(1024)
+        except socket.timeout:
+            schedule.run_pending()
+            t=datetime.datetime.now(datetime.timezone.utc)
+            T=t.astimezone().isoformat(timespec='seconds')
+            
+            if holdoff and t > holdoff:
+                print(T,"send_pagerduty_trigger")
+                event_key = event_session.trigger("Alarm is Sounding!", 'Elmhurst')
+                holdoff = 0
+            continue
+        
+        schedule.run_pending()
+
+        try: 
+            #print("recieved message:", addr, len(data), data.hex())
+            if addr[0] != BRIDGE_IP:
+                continue
+            t=datetime.datetime.now(datetime.timezone.utc)
+            T=t.astimezone().isoformat(timespec='seconds')
+
+            if len(data) > 10 and data[0:10] == b'RAKOBRIDGE':
+                print(T,"rakobridge_dhcp", data)
+                continue
+
+            if len(data) <7 or data[1] + 2 != len(data):
+                print(T,"length_error", data)
+                continue
+
+            crc = 0
+            for b in data[2:]:
+                crc += b
+            if crc != 256:
+                print(T,"CRC_fail", data)
+                continue
+
+            ctype = data[0]
+            if ctype == 0x53: # status report
+                crc = 0
+                for b in data[2:]:
+                    crc += b
+                if crc != 256:
+                    print(T,"CRC_fail", data)
+                    continue
+
+                room = data[2] * 256 + data[3]
+                channel = data[4]
+                command= data[5]
+
+                if ( len(data) == 9 or len(data) == 12) and command == 0x31: # set scene
+                    rate = data[6]
+                    val = data[7]
+                    try:
+                        roomname = xrooms[room]
+                    except:
+                        roomname = "__"
+
+                    # filter the ones that aren't real buttons
+                    if room in interactive: 
+                        I = "I"
+                        last_interactive = t
+                        if warn_alarm_unset:
+                                warn_alarm_unset = False
+                                warn_event_session.resolve(warn_event_key)
+                                print(T, "warn_alarm_resolve_interactive", warn_event_key)
+                                log_file.write( "%s warn_alarm_resolve_interactive\n" % T )
+                                warn_event_key = ''
+                    else:
+                        I = "-"
+                    
+                    risedelta=round(((t-sunrise).total_seconds())/60.0)
+                    setdelta=round(((t-sunset).total_seconds())/60.0)
+                    if ( abs(risedelta) < abs(setdelta) ):
+                        xdelta = "R%+04d" % risedelta
+                    else:
+                        xdelta = "S%+04d" % setdelta
+
+                    entry = "%s set_scene %s %s command=%02d room=%02d %s channel=%d scene=%d" % (T, xdelta, I, command, room, roomname,channel,val )
+                    print(entry)
+                    log_file.write( entry + "\n")
+
+                    if room == 52:  # alarm is room 52
+                        if val == 4:   # scene 4 is Alarm is Set
+                            if t.time().hour < 22 and t.time().hour > 6:
+                                alarm_set = "full"
+                            else:
+                                alarm_set = "night" # we don't really know this, but assume so for the moment and let 0620 schedule adjust
+
+                            print(T, "alarm_set", alarm_set)
+                            log_file.write( "%s alarm_set\n" % T ) 
+                            resp = log_session.submit("Alarm set "+ alarm_set, 'Elmhurst')
+
+                            if warn_alarm_unset:
+                                warn_alarm_unset = False
+                                warn_event_session.resolve(warn_event_key)
+                                print(T, "warn_alarm_resolve", warn_event_key)
+                                log_file.write( "%s warn_alarm_resolve\n" % T )
+                                warn_event_key = ''
+
+                        elif val == 0:    # Alarm is unset
+                            if event_key:
+                                event_session.resolve(event_key)
+                                print(T, "resolve", event_key)
+                                log_file.write( "%s alarm_resolve\n" % T )
+                                event_key = ''
+                            alarm_set = "unset"
+                            print(T, "alarm_unset")
+                            log_file.write( "%s alarm_unset\n" % T )
+                            resp = log_session.submit("Alarm unset", 'Elmhurst')
+                            holdoff = 0
+
+                        elif val == 1:    # Alarm sounding
+                            print(T, "alarm_sounding")
+                            log_file.write( "%s alarm_sounding\n" % T )
+                            holdoff = t + datetime.timedelta(seconds=alarm_delay)
+                        else:
+                            print(T,"alarm_unknown")
+                            log_file.write( "%s alarm_unknown\n" % T )
+
+                elif len(data) == 7 and command == 0x0f:
+                    print(T,"fade_stop", command, room, channel)
+                elif len(data) == 8 and command == 0x32:
+                    print(T,"fade_start", command, room, channel)
+                else:
+                    print(T,"status_parse_err", len(data), data.hex())
+            else:
+                print(T,"not_status_update", len(data), data.hex())
+    
+        except Exception as e:  
+            print(e) 
+            traceback.print_exc()
+            
+        # bottom of main while loop
+
+new_file()
+schedule.every().day.at("00:00").do(new_file)
+#schedule.every().minute.at(':00').do(new_file)
+
+get_sunrise_and_set()
+schedule.every().day.at("00:00").do(get_sunrise_and_set)
+
+schedule.every().day.at("06:20").do(check_alarm_still_set_at_0620)
+
+
+
+while True:                
+    try:
+        get_room_names()
+        print("interactive", interactive)
+        listening()  # just restart the listening loop if there are random failures
+    except Exception as e:
+        print(e)
+        traceback.print_exc
+        t=datetime.datetime.now(datetime.timezone.utc)
+        T=t.astimezone().isoformat(timespec='seconds')
+        
+        print(T,"exception_restart")
+        time.sleep(10)
+
+def set_scene( room, channel, scene):
+
+    data = bytearray.fromhex('000000000000000000')
+
+    data[0] = 0x52
+    data[1] = 7
+    data[2] = room >> 8
+    data[3] = room & 0xff
+    data[4] = channel
+    data[5] = 0x31
+    data[6] = 0x1
+    data[7] = scene
+
+    sum = 0
+    for x in data[1:]:
+        sum += x
+
+    data[8] = 0x100 - (sum & 0xff)
+
+    print(data)
+    soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+    soc.connect((BRIDGE_IP, PORT))
+    soc.send(data)
+    soc.close()
+
